6a179e91bcd4abec7fbb5d231be5ee3b6892c6e5
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-24 22:03:11 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
-import './App.css'
+const routes =(
+  <Router>
+    <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/sign-in" element={<Signin/>} />
+          <Route path="/sign-up" element={<Signup/>} />
+          <Route path="/profile" element={<Profile/>} />
+      </Routes>
+  </Router>
+);
+
+  
 
 function App() {
   
 
   return (
-    <>
-      <h1 className='bg-red-400'>d</h1>
-    </>
+   <div>{routes}</div>
   )
 }
 
