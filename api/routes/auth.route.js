9d515b0c722d3eb512084a52ae26690920624eb1
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-28 00:05:16 +0530

    hashed password

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs'; // Import bcryptjs
+
 
 const router = express.Router();
 
@@ -11,15 +13,24 @@ router.post('/signup', async (req, res) => {
 //console.log(req.body);
 
  const {username, email, password} = req.body;
+ const hashedPassword = bcryptjs.hashSync(password, 10);
  const newUser = new User ({
     username,
     email,
-    password,
+    password : hashedPassword
  });
+
+ try{
    await newUser.save();
    res.status(201).json({
        message: 'User created successfully!',
    });
+ }catch(err){
+     res.status(500).json({
+         message: 'Something went wrong!',
+     });
+ }
+   
 
 });
 
