14e00118b145e4408108c4f780d04774d639070c
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-27 23:51:41 +0530

    user create done

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,19 +3,23 @@ import User from '../models/user.model.js';
 
 const router = express.Router();
 
+
 //signup Api
 
 router.post('/signup', async (req, res) => {
 
 //console.log(req.body);
 
- const {name, email, password} = req.body;
+ const {username, email, password} = req.body;
  const newUser = new User ({
-    name,
+    username,
     email,
     password,
  });
-   await newUser.save()
+   await newUser.save();
+   res.status(201).json({
+       message: 'User created successfully!',
+   });
 
 });
 
