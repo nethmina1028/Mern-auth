1ec5df3bc7631e392d1875d85c6ae591f12f3f48
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-29 14:33:22 +0530

    Signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Signup() {
+          const [formData, setFormData] = useState({});
+          const [error, setError] = useState(false);
+          const [loading, setLoading] = useState(false);
+
+          const handleChange = (e) => {
+            setFormData({...formData, [e.target.id]: e.target.value});
+            
+          };
+
+          const handleSubmit = async (e) => {
+             e.preventDefault();
+          
+            try{
+              setLoading(true);
+              setError(false);
+              
+              const res = await fetch('api/auth/signup', {  //when using axios we dont want this below code part
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+                
+             } );
+             
+              const data = await res.json();
+              setLoading(false);
+              if(data.success === false){
+                setError(true);
+                return;
+              }
+              
+              
+             // console.log(data);
+            } catch (error) {
+              
+              setLoading(false);
+              setError(true);
+              
+            }
+            
+             
+          };
+
+  
   return (
     <div className='max-w-lg p-3 mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Sign Up</h1>
       
-      <form  className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Username'
           id='username'
           className='p-3 rounded-lg bg-slate-100'
+          onChange={handleChange}
           
         />
         <input
@@ -19,6 +65,7 @@ function Signup() {
           placeholder='Email'
           id='email'
           className='p-3 rounded-lg bg-slate-100'
+          onChange={handleChange}
          
         />
         <input
@@ -26,11 +73,13 @@ function Signup() {
           placeholder='Password'
           id='password'
           className='p-3 rounded-lg bg-slate-100'
+          onChange={handleChange}
          
         />
         
-        <button className ='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80' >
-            Sign up
+        <button disabled={loading} className ='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80' >
+          
+              {loading ? 'Loading...' : 'Sign Up'}
         </button>
         
       </form>
@@ -41,7 +90,7 @@ function Signup() {
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
-      <p className='mt-5 text-red-700'></p>
+      <p className='mt-5 text-red-700'>{error && 'Somethig went Wrong!'}</p>
     </div>
   )
 }
