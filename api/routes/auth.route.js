3e18fc3eacfb0051e19a9c3f1d1613e0ba9e9321
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-31 19:26:48 +0530

    Create Signin Api rotes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,34 +1,61 @@
 import express from 'express';
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'; // Import bcryptjs
-
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 
 //signup Api
 
-router.post('/signup', async (req, res,next) => {
+          router.post('/signup', async (req, res,next) => {
 
-//console.log(req.body);
+          //console.log(req.body);
 
- const {username, email, password} = req.body;
- const hashedPassword = bcryptjs.hashSync(password, 10);
- const newUser = new User ({
-    username,
-    email,
-    password : hashedPassword
- });
+           const {username, email, password} = req.body;
+           const hashedPassword = bcryptjs.hashSync(password, 10);
+           const newUser = new User ({
+              username,
+              email,
+              password : hashedPassword
+           });
+          
+         try{
+             await newUser.save();
+             res.status(201).json({
+                 message: 'User created successfully!',
+             });
+           }catch(err){
+               next(err);
+               }
+           
+          });
 
- try{
-   await newUser.save();
-   res.status(201).json({
-       message: 'User created successfully!',
-   });
- }catch(err){
-     next(err);
-     }
- 
-});
+
+
+                //sign in Api
+
+            router.post('/signin', async (req, res, next) => {
+
+              const {email, password} = req.body;
+              try{
+                const validUser = await User.findOne({email});
+                if(!validUser) return next(errorHandler(404, 'user not found'));
+                const validPassword = bcryptjs.compareSync(password, validUser.password);
+                if(!validPassword) return next(errorHandler(401, 'Invalid credentials'));
+                const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET );
+                const {password: hashedPassword, ...rest} = validUser._doc; //hide password from response
+                const expiryDate = new Date(Date.now() + 3600000); //1 hour
+                res
+                .cookie('access_token',token,{ httpOnly:true, expires: expiryDate})
+                .status(200)
+                .json(rest);  //send user data rest (validUser)
+
+              }catch(err){
+                next(err);
+              }
+
+            });
 
 export default router;
